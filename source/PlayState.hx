64811a1d682d69c936012edd0e57c1abab0dcc4c
MaysLastPlay10 <109207233+MaysLastPlay10@users.noreply.github.com>
2023-03-26 10:08:56 +0300

    update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2177,7 +2177,7 @@ class PlayState extends MusicBeatState
 
 		#if mobile
 		if (SONG.song.toLowerCase()=='triple-trouble') {
-		addMobileControls(true);
+		addMobileControls(usesDodge);
 		}else{
 		addMobileControls(false);  
 		}
@@ -4743,6 +4743,8 @@ class PlayState extends MusicBeatState
 				holdControls = [left, down, FlxG.keys.pressed.SPACE, up, right];
 	       if(ClientPrefs.mariomaster) //dont ask, thanks
 		{
+			if (SONG.isRing)
+				holdControls = [controls.NOTE_LEFT_P, controls.NOTE_DOWN_P, controls.NOTE_SPACE, controls.NOTE_UP_P, controls.NOTE_RIGHT_P];
 			var controlArray:Array<Bool> = [controls.NOTE_LEFT_P, controls.NOTE_DOWN_P, controls.NOTE_UP_P, controls.NOTE_RIGHT_P];
 			if(controlArray.contains(true))
 			{
